15e16dc4c0cb8ec415191bf912d359c0690c5ef9
LetzterMensch <letrongnghiavanyen@gmail.com>
2023-01-31 00:42:59 +0000

    Add avatr on users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :age, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :age, :avatar, :password, :password_confirmation])
 
     devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :email, :password, :remember_me])
 
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :avatar, :password, :password_confirmation, :current_password])
   end
 end
